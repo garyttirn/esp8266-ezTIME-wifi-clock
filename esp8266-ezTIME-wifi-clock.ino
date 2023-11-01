42e621759fa832c5da677931848e58355b3499fb
Kari Tiirikainen <kari.tiirikainen@gmail.com>
2023-11-01 19:02:39 +0200

    Add MajicDesigns Parola & MAX72xx LED matrix library for four 8x8 LED Matrix display https://github.com/MajicDesigns/MD_Parola/tree/main https://github.com/MajicDesigns/MD_MAX72XX
     Changes to be committed:
    	modified:   esp8266-ezTIME-wifi-clock.ino

--- esp8266-ezTIME-wifi-clock.ino
+++ esp8266-ezTIME-wifi-clock.ino
@@ -1,27 +1,47 @@
 #include <ESP8266WiFi.h>
-#include <esp8266-hw-spi-max7219-7seg.h>
 #include <ezTime.h>
+#include <MD_Parola.h>     // include MajicDesigns Parola library
+#include <MD_MAX72xx.h>    // include MajicDesigns MAX72xx LED matrix library
+#include <SPI.h>
 
 // configuration
-static const char ourtimezone[] PROGMEM = "Europe/Prague";  //official timezone names https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-#define wifi_ssid             "network-name"                //ssid of your WiFi
-#define wifi_pwd              "password"                    //password for your WiFi
-#define SPI_SPEED             8000000                       //display SPI@8MHZ
-#define SPI_CSPIN             5                             //display SPI CS=GPIO5
-#define DISP_BRGTH            8                             //brightness of the display (0-15)
-#define DISP_AMOUNT           2                             //number of display modules connected
+static const char ourtimezone[] PROGMEM = "Europe/Helsinki";  //official timezone names https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+#define wifi_ssid "ssid" //ssid of your WiFi
+#define wifi_pwd "password" //password for your WiFi
 
-BgrMax7seg disp = BgrMax7seg(SPI_SPEED, SPI_CSPIN, DISP_AMOUNT); //init display
+#define DISP_BRGTH 5                         // brightness of the display (0-15)
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW    // this line defines our dot matrix hardware type MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4                        // define number of total cascaded modules
+#define DATA_PIN 13   // define Data pin
+#define CLK_PIN 14   // define Clock pin
+#define CS_PIN 15   // define CS (Chip Select) pin for WEMOS D1 MINI
+
+uint8_t scrollSpeed = 150;    // set initial scroll speed, can be a value between 10 (max) and 150 (min)
+textEffect_t scrollEffect  = PA_PRINT;  // Scroll disabled
+textPosition_t scrollAlign = PA_CENTER; // scroll align
+uint16_t scrollPause = 2000;            // scroll pause in milliseconds
+
+char _chbuf[16] = {"READY"};
+
+// Software SPI with custom PINs
+// MD_Parola display = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+//Hardware SPI 
+MD_Parola display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+
 Timezone myTZ;
 
 void setup() {
   Serial.begin(115200);
   Serial.println(F("\n* * * ESP BOOT * * *"));
   setDebug(INFO);
-  disp.init();
-  disp.setBright(DISP_BRGTH, ALL_MODULES);
-  disp.print(F("NTPclock"), 1);
-  disp.print(F("bugerdrd"), 2);
+
+  Serial.println(F("Display Init"));
+  // initialize the dot matrix display
+  display.begin();
+  // set the intensity (brightness) of the display
+  display.setIntensity(DISP_BRGTH);
+  Serial.println(F("Display Ready"));
   
   Serial.println(F("Connecting to WiFi"));
   WiFi.begin( wifi_ssid, wifi_pwd);
@@ -30,7 +50,9 @@ void setup() {
     Serial.print(".");
   }
   Serial.println(F("\nWiFi connected"));
-  
+
+  //NTP Server
+  setServer("pool.ntp.org");
   waitForSync();
 
   if (myTZ.setLocation(FPSTR(ourtimezone))) {
@@ -39,12 +61,23 @@ void setup() {
     Serial.println(F("\nTimezone lookup failed, will use UTC!"));
     myTZ = UTC;
   }
+  display.displayText(_chbuf, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+  display.displayAnimate();
 }
 
 void loop() {
   events();
-  if (secondChanged()) {
-    disp.print(myTZ.dateTime("H-i-s"), 1);
-    disp.print(myTZ.dateTime("d.m.Y"), 2);
+  if (minuteChanged()) {
+    // Length (with one extra character for the null terminator)
+    int str_len = myTZ.dateTime("H:i").length() + 1; 
+    myTZ.dateTime("H:i").toCharArray(_chbuf,str_len);
+    // print text on the display
+    display.displayText(_chbuf, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+
+    Serial.printf("\nTime: %s",_chbuf);
+  
+    if (display.displayAnimate()) {    // animate the display
+      display.displayReset();          // reset the current animation
+    }
   }
 }
